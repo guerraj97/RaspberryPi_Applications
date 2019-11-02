412a7991751ade40c275a2febc6465310581058e
guerraj97 <gue16242@uvg.edu.gt>
2019-11-01 20:09:13 -0600

    Create GoogleC.c

---
+++ GoogleC.c
@@ -0,0 +1,11 @@
+/*
+Aplicacion futura para el control del Calendario de Google con las RaspberryPi
+Autor: Jose Pablo Guerra
+Fecha de inicio: Noviembre 1, 2019
+*/
+
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
